58bdf962f99f82108828804d9b6bae4ae9160e99
Saroj Kumar Pradhan <70810569+Saroj-Pradhan@users.noreply.github.com>
2020-09-08 00:28:41 +0530

    adding some Full stops

--- Find_Average_of_10_Numbers.py
+++ Find_Average_of_10_Numbers.py
@@ -1,11 +1,11 @@
-#Take 10 numbers from the user and find average value of the entered number using WHILE LOOP
+#Take 10 numbers from the user and find average value of the entered number using WHILE LOOP .
 sum=0
 limit=10
 no=1
 
 while (limit>0):
 
-#Here user will input the numbers for 10 times
+#Here user will input the numbers for 10 times .
     userinput=int(input("Enter no %d: "%no))
 
 #Algorithm lines (12,13,14,15)
