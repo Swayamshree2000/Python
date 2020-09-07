ddbd139aa9874e39aee39afa5a82440c3e539e8d
Saroj Kumar Pradhan <70810569+Saroj-Pradhan@users.noreply.github.com>
2020-09-07 23:31:04 +0530

    Change Docs

--- Average_of_Number_Using_While-Loop.py
+++ Average_of_Number_Using_While-Loop.py
@@ -1,4 +1,4 @@
-#Take 10 numbers from the user and find average value of the entered number using WHILE LOOP
+#Take 10 numbers from the user and find average value of the entered number using WHILE LOOP .
 sum=0
 limit=10
 #while (limit>0):
