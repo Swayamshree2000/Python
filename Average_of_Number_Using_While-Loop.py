e394fc4a9c2b883f3b174b0a6f6676981f852b53
Swayamshree Mohanty <mohantyswayamshree@gmail.com>
2020-09-07 23:39:00 +0530

    final answer

--- Average_of_Number_Using_While-Loop.py
+++ Average_of_Number_Using_While-Loop.py
@@ -1,7 +1,11 @@
 #Take 10 numbers from the user and find average value of the entered number using WHILE LOOP
 sum=0
 limit=10
-#while (limit>0):
-for i in range(limit):
-    print(limit)
+no=1
+while (limit>0):
+    userinput=int(input("Enter no %d: "%no))
+    sum=sum+userinput
     limit-=1
+    no+=1
+avg=sum/10
+print("The Average is %d"%avg)
