f270fd3733814d3f42b0ac6a9e2f024fd6cb0e59
Swayamshree Mohanty <mohantyswayamshree@gmail.com>
2020-09-08 21:51:03 +0530

    Average of 10 Numbers using While Loop

--- Find_Average_of_10_Numbers(Using_While-Loop).py
+++ Find_Average_of_10_Numbers(Using_While-Loop).py
@@ -1,19 +1,18 @@
 #Take 10 numbers from the user and find average value of the entered number using WHILE LOOP
 sum=0
 limit=10
-no=1
+number=1
 
 #This WHILE-LOOP statement helps the loop to run 10 times
 while (limit>0):
 
 #Here user will input the numbers for 10 times
-    userinput=int(input("Enter no %d: "%no))
+    userinput=int(input("Enter no %d: "%number))
 
-#Algorithm lines (12,13,14,15)
+#Algorithm lines (13,14,15)
     sum=sum+userinput
     limit-=1
-    no+=1
-avg=sum/10                    
-
+    number+=1
+                  
 #This is the final output.
-print("The Average is %d"%avg)
+print("The Average is %d"%(sum/10))
