8f7177b75c1a66ce757c64dd52e2e467ae5661ee
Swayamshree Mohanty <mohantyswayamshree@gmail.com>
2020-09-08 21:50:44 +0530

    Average of 10 Numbers using FOR Loop

--- Find_Average_of_10_Numbers(Using_For-Loop).py
+++ Find_Average_of_10_Numbers(Using_For-Loop).py
@@ -1,18 +1,18 @@
 #Take 10 numbers from the user and find average value of the entered number using WHILE LOOP
 sum=0
 limit=10
-no=1
+number=1
 
 #This FOR-LOOP statement helps the loop to run 10 times
 for i in range(limit):
 
 #Here user will input the numbers for 10 times
-    userinput=int(input("Enter no %d: "%no))
+    userinput=int(input("Enter no %d: "%number))
 
 #Algorithm lines (12,13,14,15)
     sum=sum+userinput
     limit-=1
-    no+=1
+    number+=1
 avg=sum/10                    
 
 #This is the final output.
