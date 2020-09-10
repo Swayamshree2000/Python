0dbfdb5a241b6c2a9e8fb813b425cb82a736fddf
Swayamshree Mohanty <mohantyswayamshree@gmail.com>
2020-09-10 21:51:30 +0530

    multiplication table of 14 from a list in which multiplication table of 12 is stored

---
+++ multiplication_table_of_14_from_12.py
@@ -0,0 +1,17 @@
+#Print multiplication table of 14 from a list in which multiplication table of 12 is stored. (using for loop)
+
+#12's Table
+table1=[12,24,36,48,60,72,84,96,108,120]
+
+#variable for 14's table
+start=14
+multiply=1
+
+#for increament of 2 in each loop
+a=2
+
+#Algorith using FOR-LOOP
+for i in table1:
+    print("%d X %d = %d"%(start,multiply,(i+a)))
+    a+=2
+    multiply+=1
